6a46dfbcf038b67f1c391ba2b5c4dbabe77d6c67
macbook <macbook@192.168.1.3>
2018-02-21 23:38:14 +0700

    new commit

---
+++ src/pokemongame/Charmander.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemongame;
+
+/**
+ *
+ * @author macbook
+ */
+class Charmander extends Pokemon {
+    public Charmander(String name, float weight, float stepLength){
+	super(name, weight, stepLength, 1f, new String[] {"fire"});
+    }
+}
