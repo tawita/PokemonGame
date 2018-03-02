ef907a856e6385ec3030407a32c72fc7edac1bb0
macbook <macbook@192.168.1.3>
2018-03-02 23:24:52 +0700

    new commit

---
+++ src/pokemongame/Charmeleon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemongame;
+
+/**
+ *
+ * @author macbook
+ */
+public class Charmeleon extends Charmander {
+	public Charmeleon(String name, float weight, float stepLength,int level){
+		super(name, weight, stepLength,level);
+               
+	}
+    
+}
